fddf4879cac8e8abcb4c9837648e833b45630155
alexandr-turbo <turbo.xxxxxxxxxx@gmail.com>
2019-11-27 16:55:00 +0200

    8

--- src/components/FilmService.vue
+++ src/components/FilmService.vue
@@ -1,10 +1,24 @@
 <template>
-  <div class="container" v-if="text != null">
-    <slick ref="slick"  :options="slickOptions" >
-
-        <img :src="`https://image.tmdb.org/t/p/w500${text.poster_path}`">
-
-        <img :src="`https://image.tmdb.org/t/p/w500${text.poster_path}`">
+  <div class="container" v-if="upcoming != null && popular != null && now_playing != null && top_rated != null ">
+    <slick class="slick" ref="slick"  :options="slickOptions" >
+      <div v-for="item in upcoming" v-bind:key="item.id">
+        <img :src="`https://image.tmdb.org/t/p/w92${item.poster_path}`">
+      </div>
+    </slick>
+    <slick class="slick" ref="slick"  :options="slickOptions" >
+      <div v-for="item in popular" v-bind:key="item.id">
+        <img :src="`https://image.tmdb.org/t/p/w92${item.poster_path}`">
+      </div>
+    </slick>
+    <slick class="slick" ref="slick"  :options="slickOptions" >
+      <div v-for="item in now_playing" v-bind:key="item.id">
+        <img :src="`https://image.tmdb.org/t/p/w92${item.poster_path}`">
+      </div>
+    </slick>
+    <slick class="slick" ref="slick"  :options="slickOptions" >
+      <div v-for="item in top_rated" v-bind:key="item.id">
+        <img :src="`https://image.tmdb.org/t/p/w92${item.poster_path}`">
+      </div>
     </slick>
   </div>
 </template>
@@ -15,18 +29,34 @@ import Slick from 'vue-slick';
 export default {
   data() {
     return {
-      text: null,
+      upcoming: null,
+      popular: null,
+      now_playing: null,
+      top_rated: null,
       slickOptions: {
         //options can be used from the plugin documentation
-        slidesToShow: 1,
-        infinite: true
+        slidesToShow: 7,
+        infinite: true,
+        autoplay: true
       }
     }
   },
   created() {
-    axios.get('https://api.themoviedb.org/3/movie/551?api_key=f943d3d10cc39fd734122d69efabbacb')
+    axios.get('https://api.themoviedb.org/3/movie/upcoming?api_key=f943d3d10cc39fd734122d69efabbacb')
     .then(response => {
-      this.text = response.data
+      this.upcoming = response.data.results
+    }),
+    axios.get('https://api.themoviedb.org/3/movie/popular?api_key=f943d3d10cc39fd734122d69efabbacb')
+    .then(response => {
+      this.popular = response.data.results
+    }),
+    axios.get('https://api.themoviedb.org/3/movie/now_playing?api_key=f943d3d10cc39fd734122d69efabbacb')
+    .then(response => {
+      this.now_playing = response.data.results
+    }),
+    axios.get('https://api.themoviedb.org/3/movie/top_rated?api_key=f943d3d10cc39fd734122d69efabbacb')
+    .then(response => {
+      this.top_rated = response.data.results
     })
   },
   components: {
@@ -50,5 +80,14 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .container {
+    width: 1500px;
+    margin-left: auto;
+    margin-right: auto;
 
+  }
+  img {
+    width: 92px !important;
+  }
+  
 </style>
