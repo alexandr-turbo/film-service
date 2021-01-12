86aeca07400d16bb740f581359780a3c9b7efd21
Oleksandr <oleksandrsavostian@gmail.com>
2021-01-12 16:58:46 +0200

    publish

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-    // indexPath: '/dist/index.html'
-    // publicPath: '/film-service/'
-    // publicPath: '/'
-    // publicPath: './'
-    publicPath: 'https://alexandr-turbo.github.io/film-service/'
-}
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/film-service/'
+      : '/'
+  }
